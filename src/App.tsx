923b64302318451026993be0e096cbcd5b289c87
zaw.w.lwin <zaw.w.lwin@heimdallmedical.com>
2025-01-11 04:09:39 +0700

    feat[Auth]: Add auth0 provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import { AUTH } from "@/config/auth";
+
 import Router from "./router";
 
 function App() {
-  return <Router />;
+  return (
+    <Auth0Provider
+      domain={AUTH.AUTH0_DOMAIN}
+      clientId={AUTH.AUTH0_CLIENT_ID}
+      authorizationParams={{ redirect_uri: AUTH.AUTH0_REDIRECT_URI }}
+    >
+      <Router />
+    </Auth0Provider>
+  );
 }
 
 export default App;
---
+++ src/config/auth.ts
@@ -0,0 +1,5 @@
+export const AUTH = {
+  AUTH0_DOMAIN: import.meta.env.VITE_AUTH0_DOMAIN,
+  AUTH0_CLIENT_ID: import.meta.env.VITE_AUTH0_CLIENT_ID,
+  AUTH0_REDIRECT_URI: import.meta.env.VITE_AUTH0_REDIRECT_URI,
+} as const;
