c74e3ff4db40f7ecc45e95b99b9f0e490a41f3c4
zaw.w.lwin <zaw.w.lwin@heimdallmedical.com>
2025-01-12 17:30:28 +0700

    feat[Sidebar]: Render skeleton before receiving access token

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,4 +1,6 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { LayoutDashboard, Users } from "lucide-react";
+import { useCallback, useEffect, useState } from "react";
 
 import { ROUTES } from "@/config/routes";
 
@@ -14,6 +16,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "./ui/sidebar";
+import { Skeleton } from "./ui/skeleton";
 
 const items = [
   {
@@ -29,30 +32,82 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const { getAccessTokenSilently } = useAuth0();
+  const [accessToken, setAccessToken] = useState("");
+
+  const getToken = useCallback(async () => {
+    const token = await getAccessTokenSilently();
+    setAccessToken(token);
+  }, [getAccessTokenSilently]);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies:
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  const renderLocationSwitcher = () => {
+    if (accessToken) {
+      return <LocationSwitcher />;
+    }
+    return (
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-full" />
+        <div className="w-3/4 space-y-2">
+          <Skeleton className="h-4" />
+          <Skeleton className="h-4 w-5/6" />
+        </div>
+      </div>
+    );
+  };
+
+  const renderNavItems = () => {
+    if (accessToken) {
+      return items.map((item) => (
+        <SidebarMenuItem key={item.title}>
+          <SidebarMenuButton asChild>
+            <a href={item.url}>
+              <item.icon />
+              <span>{item.title}</span>
+            </a>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ));
+    }
+
+    return Array.from({ length: 2 }).map((_item, index) => (
+      // biome-ignore lint/suspicious/noArrayIndexKey:
+      <div className="flex items-center space-x-4" key={index}>
+        <Skeleton className="h-8 w-8 rounded-xl" />
+        <Skeleton className="h-4 w-5/6" />
+      </div>
+    ));
+  };
+
+  const renderFooter = () => {
+    if (accessToken) {
+      return <Account />;
+    }
+
+    return (
+      <div className="flex items-center space-x-4">
+        <Skeleton className="h-12 w-12 rounded-xl" />
+        <div className="w-3/4 space-y-2">
+          <Skeleton className="h-4" />
+          <Skeleton className="h-4 w-5/6" />
+        </div>
+      </div>
+    );
+  };
+
   return (
     <Sidebar>
-      <SidebarHeader>
-        <LocationSwitcher />
-      </SidebarHeader>
+      <SidebarHeader>{renderLocationSwitcher()}</SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarMenu className="px-2">
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild>
-                <a href={item.url}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </a>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarMenu className="px-2">{renderNavItems()}</SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter>
-        <Account />
-      </SidebarFooter>
+      <SidebarFooter>{renderFooter()}</SidebarFooter>
     </Sidebar>
   );
 }
